b374cbd4e3eec6595cb9676d42ae8c2dabc5ef60
daemon-cool <damian.damian.tofik@gmail.com>
2020-05-28 18:45:08 +0200

    added variable that can be chenaged from console, updated session set

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const TWO_HOURS = 1000 * 60 * 60 * 2;
 
 const {
     PORT = 3000,
+    NODE_ENV = 'development',
 
+    SESS_NAME = 'sid',
+    SESS_SECRET = 'ssh!quiet,it\'asecret',
     SESS_LIFETIME = TWO_HOURS
 } = process.env;
+
+const IN_PROD = NODE_ENV === 'production';
 
 const app = express();
 
 app.use(session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
     cookie: {
-        maxAge: SESS_LIFETIME
+        maxAge: SESS_LIFETIME,
+        sameSite: true,
+        secure: IN_PROD,
     }
 }));
 
