a47b231caf438db65163691bbd4904896596b9a1
daemon-cool <damian.damian.tofik@gmail.com>
2020-05-28 18:32:22 +0200

    added session and lifetime of session

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const session = require('express-session');
 
+const TWO_HOURS = 1000 * 60 * 60 * 2;
+
 const {
-    PORT = 3000
+    PORT = 3000,
+
+    SESS_LIFETIME = TWO_HOURS
 } = process.env;
 
 const app = express();
+
+app.use(session({
+    cookie: {
+        maxAge: SESS_LIFETIME
+    }
+}));
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
